893c8229526fecb52dc8da3e789bd17537ecd5f3
NoNameNeeded1 <67867326+NoNameNeeded1@users.noreply.github.com>
2020-07-14 23:33:43 +0700

    Add files via upload

---
+++ Source/Alien_invader1/Alien_invader1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Alien_invader1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Alien_invader1, "Alien_invader1" );
---
+++ Source/Alien_invader1/Alien_invader1GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Alien_invader1GameModeBase.h"
+
