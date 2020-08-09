6d49b341670841bf62472e3c2d4bc8a18d514426
NoNameNeeded1 <67867326+NoNameNeeded1@users.noreply.github.com>
2020-08-09 21:37:19 +0700

    Delete Alien_invader1.cpp

--- Source/Alien_invader1/Alien_invader1.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Alien_invader1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Alien_invader1, "Alien_invader1" );
